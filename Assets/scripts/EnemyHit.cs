321abed58bffaee01072df5ff4dfa147d97bf335
roadstobe <epro100live@gmail.com>
2020-05-21 20:17:26 +0300

    Initial commit

---
+++ Assets/scripts/EnemyHit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHit : MonoBehaviour
+{
+    public int _maxHealth;
+    public int _health;
+    public int _gold;
+    GameManager _gameManager;
+    public bool hasDead = false;
+
+    private void Start()
+    {
+        _gameManager = FindObjectOfType<GameManager>();
+    }
+    private void OnMouseDown()
+    {
+        _gameManager.Player.GetComponent<Animator>().SetTrigger("Attack");  
+        GetComponent<Animator>().SetTrigger("Hurt");
+        GetHurt(_gameManager._damage);
+    }
+
+    private void GetHurt(int damage)
+    {
+        int health = _health - damage;
+        if(health <= 0 && !hasDead)
+        {
+            hasDead = true;
+            GetComponent<Animator>().SetTrigger("Death");
+            Destroy(gameObject, 0.7f);
+            _gameManager.EnemyDeath(_gold);
+           
+        }
+        _health = health;
+        Debug.Log(_health);
+    }
+}
---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public int playerGold;
+    public int _damage;
+    public Text goldUI;
+    public GameObject Player;
+    public GameObject[] _enemys;
+    public Transform _spawnPosition;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("PlayerGold"))
+        {
+            playerGold = (int)PlayerPrefs.GetFloat("PlayerGold");
+        }
+
+       
+    }
+    private void Update()
+    {
+        goldUI.text = "GOLD: " + playerGold.ToString();
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //PlayerPrefs.DeleteKey("PlayerGold");
+            Time.timeScale = 0;
+        }
+        
+    }
+    public void EnemyDeath(int gold)
+    {
+        playerGold += gold;
+        Invoke("SpawnEnemy", 1f);
+        PlayerPrefs.SetFloat("PlayerGold", playerGold);
+    }
+    public void SpawnEnemy()
+    {
+        int index = Random.Range(0, _enemys.Length);
+
+        GameObject enemy = Instantiate(_enemys[index]);
+        enemy.transform.position = _spawnPosition.position;
+    }
+}
